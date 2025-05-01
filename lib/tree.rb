4b404c418bd39a8a9d377b9094131b557b3a31be
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-01 14:20:15 -0600

    feat: add method to build complete binary tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Tree class builds a balanced
 # binary search tree. It contains
 # methods to manipulate data,
@@ -17,10 +19,18 @@ class Tree
   attr_accessor :root
 
   def initialize(array = [])
-    @root = build_tree
+    @root = build_tree(array.sort.uniq)
   end
 
-  def build_tree
-    
+  def build_tree(array)
+    start_index = 0
+    end_index = array.length - 1
+    return nil if array.empty?
+
+    middle_index = (start_index + end_index) / 2
+    root = Node.new(array[middle_index])
+    root.left = build_tree(array[0...middle_index])
+    root.right = build_tree(array[(middle_index + 1)..])
+    root
   end
 end
