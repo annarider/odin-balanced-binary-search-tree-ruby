8c13534e0ffddf7d4a3ccfc20118aafa12682935
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-01 13:15:18 -0600

    feat: add comparable module for comparing nodes

--- lib/node.rb
+++ lib/node.rb
@@ -4,12 +4,22 @@
 # Tree class. It contains
 # the data value and points to
 # the left and right child.
+# 
+# It relies on the Comparable
+# module (built into Ruby)
+# to compare the data of 2 nodes.
 #
 # @example Create a new Node
 # node = Node.new
 #
 class Node
+  include Comparable
+
   attr_accessor :data, :left, :right
+
+  def <=>(node)
+    data <=> node.data
+  end
 
   def initialize(data = nil, left = nil, right = nil)
     @data = data
